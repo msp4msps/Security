6ae274907cbd550f92043554370149868bda1eeb
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-10 15:03:17 -0600

    Add files via upload

---
+++ Block Admin-MFA.ps1
@@ -0,0 +1,7 @@
+﻿Connect-MsolService
+$admins = Import-csv C:\temp\MFAEnabledAdmins.csv
+ 
+foreach($admin in $admins){
+    Write-Host "Blocking $($admin.userprincipalname)"
+    Set-Msoluser -tenantid $admin.tenantid -userprincipalname $admin.userprincipalname -blockcredential $true
+}
