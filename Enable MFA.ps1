0a2194ba8e220559a57a0ed92c17b1e8056fc83e
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-10 14:57:18 -0600

    Add files via upload

---
+++ Enable MFA.ps1
@@ -0,0 +1,16 @@
+﻿$UserCredential = Get-Credential
+
+Import-Module MSOnline
+
+Connect-MsolService –Credential $UserCredential
+
+$auth = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+
+$auth.RelyingParty = "*"
+
+$auth.State = "Enabled"
+
+$auth.RememberDevicesNotIssuedBefore = (Get-Date)
+
+Get-MsolUser –All | Foreach{ Set-MsolUser -UserPrincipalName $_.UserPrincipalName -StrongAuthenticationRequirements $auth}
+
