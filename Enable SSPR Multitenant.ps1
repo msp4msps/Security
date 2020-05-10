0565551d46efe0e09a647674ddab771be96808a1
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-10 15:03:37 -0600

    Add files via upload

---
+++ Enable SSPR Multitenant.ps1
@@ -0,0 +1,7 @@
+﻿﻿#Connect to Office365 Partner Tenancy
+$Cred = Get-Credential
+Connect-MsolService -Credential $Cred
+#Get list of Tennant ID's
+$Tenant = Get-MsolPartnerContract
+foreach ($ID in $Tenant) {Set-MsolCompanySettings -TenantId $ID.TenantID -SelfServePasswordResetEnabled $true
+Get-MsolCompanyInformation -TenantId $ID.TenantId | Select DisplayName, SelfServePasswordResetEnabled}
