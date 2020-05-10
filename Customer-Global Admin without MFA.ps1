f4f17cb1a40241d4661df3e77bd5c29ab3240c33
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-10 15:01:04 -0600

    Add files via upload

---
+++ Customer-Global Admin without MFA.ps1
@@ -0,0 +1,24 @@
+﻿Connect-MsolService
+ 
+$customers = Get-MsolPartnerContract
+$role = Get-MsolRole | Where-Object {$_.name -contains "Company Administrator"}
+foreach($customer in $customers){
+     
+    $users = Get-MsolUser -TenantId $customer.tenantid
+    $admins = Get-MsolRoleMember -TenantId $customer.tenantid -RoleObjectId $role.objectid
+ 
+    foreach($admin in $admins){
+        $adminuser = $users | Where-Object {$_.userprincipalname -contains $admin.emailaddress}
+        if($adminuser){
+            if($adminuser.strongauthenticationrequirements.state -notcontains "Enforced" -and $adminuser.strongauthenticationrequirements.state -notcontains "Enabled"){
+                Write-Host "No MFA enabled for $($adminuser.userprincipalname)"
+                $adminuser | Add-Member TenantId $customer.tenantid
+                $adminuser | Add-Member CustomerName $customer.name
+                $adminuser | Select-Object TenantId,CustomerName,DisplayName,UserPrincipalName,islicensed,@{n="MFAStatus";e={$_.strongauthenticationrequirements.state}} | export-csv C:\temp\nonMFAAdmins.csv -NoTypeInformation -Append
+ 
+            }else{
+                Write-Host "MFA enabled for $($adminuser.userprincipalname)" -ForegroundColor Green
+            }
+        }
+    }
+}
