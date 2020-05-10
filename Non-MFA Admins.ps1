db1befabbe438b86440c64cd5922d11752d4323a
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-10 14:58:22 -0600

    Add files via upload

---
+++ Non-MFA Admins.ps1
@@ -0,0 +1,22 @@
+﻿Connect-MsolService
+$admins = Import-csv C:\temp\nonMFAAdmins.csv
+ 
+$auth = New-Object -TypeName Microsoft.Online.Administration.StrongAuthenticationRequirement
+$auth.RelyingParty = "*"
+$auth.State = "Enabled"
+$auth.RememberDevicesNotIssuedBefore = (Get-Date)
+ 
+foreach ($admin in $admins) {
+ 
+    if ($admin.IsLicensed -eq "FALSE") {
+        Write-Host "Enabling MFA for $($admin.userprincipalname)" -ForegroundColor Green
+        Set-MsolUser -UserPrincipalName $admin.userprincipalname -StrongAuthenticationRequirements $auth -TenantId $admin.tenantid
+        $state = (get-msoluser -TenantId $admin.tenantid -UserPrincipalName $admin.UserPrincipalName).StrongAuthenticationRequirements.state
+        $admin.MFAStatus = $state
+        $admin | export-csv C:\temp\adminMFAStatus.csv -NoTypeInformation -append
+    }
+    else {
+        Write-Host "Not Enabling MFA for $($admin.userprincipalname)" -ForegroundColor Red
+        $admin | export-csv C:\temp\MFAEnabledAdmins.csv -Append -NoTypeInformation
+    }
+}
