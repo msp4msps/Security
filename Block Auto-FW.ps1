19fd884974fb94e54efbdb672a9d7cef433b4bdd
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-15 07:16:30 -0600

    Add files via upload

---
+++ Block Auto-FW.ps1
@@ -0,0 +1,16 @@
+﻿Install-Module PowershellGet -Force
+Update-Module PowershellGet
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+ 
+$externalTransportRuleName = "Block Auto-Forwarding"
+$rejectMessageText = "To improve security, auto-forwarding rules to external email addresses have been disabled. Please contanct your helpdesk if you want to create an exception."
+ 
+$externalForwardRule = Get-TransportRule | Where-Object {$_.Identity -contains $externalTransportRuleName}
+ 
+if (!$externalForwardRule) {
+    Write-Output "Rule for Auto-forwarding not found, creating Rule"
+    New-TransportRule -name "Block Auto-forwarding" -Priority 1 -SentToScope NotInOrganization -FromScope InOrganization -MessageTypeMatches AutoForward -RejectMessageEnhancedStatusCode 5.7.1 -RejectMessageReasonText $rejectMessageText
+}   
---
+++ Block Auto-FW_All Customers.ps1
@@ -0,0 +1,24 @@
+﻿$credential = Get-Credential
+Connect-MsolService -Credential $credential
+$customers = Get-MsolPartnerContract -All
+$TransportRuleName = "Block Auto-Forwarding"
+$rejectMessage = "To improve security, auto-forwarding rules to external email addresses have been disabled. Please contanct your helpdesk if you want to create an exception"
+ 
+Write-Output "Found $($customers.Count) customers for $((Get-MsolCompanyInformation).displayname)."
+  
+foreach ($customer in $customers) {
+    $InitialDomain = Get-MsolDomain -TenantId $customer.TenantId | Where-Object {$_.IsInitial -eq $true}
+            
+    Write-Output "Checking transport rule for $($Customer.Name)"
+    $DelegatedOrgURL = "https://outlook.office365.com/powershell-liveid?DelegatedOrg=" + $InitialDomain.Name
+    $session = New-PSSession -ConnectionUri $DelegatedOrgURL -Credential $credential -Authentication Basic -ConfigurationName Microsoft.Exchange -AllowRedirection
+    Import-PSSession $session -CommandName Get-TransportRule, New-TransportRule, Set-TransportRule -AllowClobber
+      
+    $externalForwardRule = Get-TransportRule | Where-Object {$_.Identity -contains $TransportRuleName}
+ 
+    if (!$externalForwardRule) {
+        Write-Output "Rule for Auto-forwarding not found, creating Rule"
+        New-TransportRule -name "Block Auto-forwarding" -Priority 1 -SentToScope NotInOrganization -FromScope InOrganization -MessageTypeMatches AutoForward -RejectMessageEnhancedStatusCode 5.7.1 -RejectMessageReasonText $rejectMessage
+    }    
+    Remove-PSSession $session
+}
