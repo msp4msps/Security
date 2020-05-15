2366afe3d8ce4ff2f0fe261580e39dcc6e189a80
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-15 07:15:49 -0600

    Add files via upload

---
+++ Email Encryption Rule-All Customers.ps1
@@ -0,0 +1,24 @@
+﻿$credential = Get-Credential
+Connect-MsolService -Credential $credential
+$customers = Get-MsolPartnerContract -All
+$TransportRuleName = "Email Encryption"
+
+ 
+Write-Output "Found $($customers.Count) customers for $((Get-MsolCompanyInformation).displayname)."
+  
+foreach ($customer in $customers) {
+    $InitialDomain = Get-MsolDomain -TenantId $customer.TenantId | Where-Object {$_.IsInitial -eq $true}
+            
+    Write-Output "Checking transport rule for $($Customer.Name)"
+    $DelegatedOrgURL = "https://outlook.office365.com/powershell-liveid?DelegatedOrg=" + $InitialDomain.Name
+    $session = New-PSSession -ConnectionUri $DelegatedOrgURL -Credential $credential -Authentication Basic -ConfigurationName Microsoft.Exchange -AllowRedirection
+    Import-PSSession $session -CommandName Get-TransportRule, New-TransportRule, Set-TransportRule -AllowClobber
+      
+    $EmailEncryptRule = Get-TransportRule | Where-Object {$_.Identity -contains $TransportRuleName}
+ 
+    if (!$EmailEncryptRule) {
+        Write-Output "Rule for Encryption not found, creating Rule"
+        New-TransportRule -Name "Encrypt Email" -SubjectContainsWords "Secure" -ApplyRightsProtectionTemplate "Encrypt"
+    }    
+    Remove-PSSession $session
+}
---
+++ Email Encryption Rule.ps1
@@ -0,0 +1,8 @@
+﻿Install-Module PowershellGet -Force
+Update-Module PowershellGet
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+New-TransportRule -Name "Encrypt Email" -SubjectContainsWords "Secure" -ApplyRightsProtectionTemplate "Encrypt"
