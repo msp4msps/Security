e62a444f979a96b8df57559a5a560beec543d8bd
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-16 06:48:36 -0600

    Add files via upload

---
+++ Free_Busy Calendar Settings Multitenant.ps1
@@ -0,0 +1,29 @@
+﻿$credential = Get-Credential
+Connect-MsolService -Credential $credential
+$customers = Get-MsolPartnerContract -All
+Write-Host "Found $($customers.Count) customers for this Partner."
+ 
+foreach ($customer in $customers) {
+
+
+    $InitialDomain = Get-MsolDomain -TenantId $customer.TenantId | Where-Object {$_.IsInitial -eq $true}
+
+    Write-Host "Enabling External Calendar Sharing Policy for $($Customer.Name)"
+
+    $DelegatedOrgURL = "https://outlook.office365.com/powershell-liveid?DelegatedOrg=" + $InitialDomain.Name
+    $session = New-PSSession -ConnectionUri $DelegatedOrgURL -Credential $credential -Authentication Basic -ConfigurationName Microsoft.Exchange -AllowRedirection
+    Import-PSSession $session -CommandName Get-SharingPolicy, Set-SharingPolicy  -AllowClobber
+    Enable-OrganizationCustomization
+
+    $existingsetting = Get-SharingPolicy -Identity "Default Sharing Policy"
+
+    if($existingsetting){
+
+
+    Set-SharingPolicy -Identity "Default Sharing Policy" -Domains "Anonymous: CalendarSharingFreeBusySimple"
+    Write-Host "Policy set for $($customer.Name)"
+
+    }
+    Remove-PSSession $session
+
+}
---
+++ Free_Busy Calendar Settings-Single Tenant.ps1
@@ -0,0 +1,11 @@
+﻿Function Connect-EXOnline {
+    $credentials = Get-Credential
+    Write-Output "Getting the Exchange Online cmdlets"
+    $session = New-PSSession -ConnectionUri https://outlook.office365.com/powershell-liveid/ `
+        -ConfigurationName Microsoft.Exchange -Credential $credentials `
+        -Authentication Basic -AllowRedirection
+    Import-PSSession $session
+}
+Connect-EXOnline 
+
+Set-SharingPolicy -Identity "Default Sharing Policy" -Domains "Anonymous: CalendarSharingFreeBusySimple"
