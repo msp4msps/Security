b5071fd96c81f3d5f6037b6aedaf9f4bc24f75c5
msp4msps <49374962+msp4msps@users.noreply.github.com>
2020-05-17 14:51:18 -0600

    Add files via upload

---
+++ Set Outbound Spam Notifications.ps1
@@ -0,0 +1,10 @@
+﻿Install-Module PowershellGet -Force
+Update-Module PowershellGet
+Set-ExecutionPolicy RemoteSigned
+Install-Module -Name ExchangeOnlineManagement
+
+Connect-ExchangeOnline
+
+$NotifcationEntity = Read-Host -Prompt "Enter the email of the recipient who will receive the spam notifications"
+
+Set-HostedOutboundSpamFilterPolicy Default -NotifyOutboundSpam $true -NotifyOutboundSpamRecipients $NotifcationEntity
