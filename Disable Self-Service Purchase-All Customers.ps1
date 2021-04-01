0666e9709d462d814b21605ee159e25d82f1d9f8
msp4msps <49374962+msp4msps@users.noreply.github.com>
2021-04-01 15:46:23 -0600

    Add files via upload

---
+++ Disable Self-Service Purchase-All Customers.ps1
@@ -0,0 +1,27 @@
+﻿Write-Host "Checking for MSOnline module..."
+
+$Module = Get-Module -Name "MSOnline" -ListAvailable
+
+if ($Module -eq $null) {
+    
+        Write-Host "MSOnline module not found, installing MSOnline"
+        Install-Module -name MSOnline
+    
+    }
+
+Write-Host "Please Enter your Partner Center Global Admin Credentials"
+
+Connect-MSolservice -Credential $credential
+$tenants = Get-MsolPartnerContract -All
+
+
+
+
+ForEach($tenant in $tenants){
+
+Write-Host "Disabling Self-Service for $($tenant.Name)" -ForegroundColor Green
+
+Set-MsolCompanySettings -AllowAdHocSubscriptions $false
+
+
+}
